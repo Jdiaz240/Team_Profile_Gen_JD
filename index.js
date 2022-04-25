6e2a9ee64c21309be860a999d5787da552773900
Joshua Diaz <jad24096@gmail.com>
2022-04-25 14:56:37 -0400

    added classes and constructors to lib folder files.

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-// const Manager = require('./lib/Manager');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const path = require('path');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = ({fName, position, email}) => 
 
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is your name?' ,
+        name: 'fName',
+    },
+    {
+        type: 'input',
+        message: 'What is your position?',
+        name: 'position',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    
+
+  ])
+  .then((data) => {
+    const readMeContent = generateReadMe(data)
+    fs.writeFile ('readme.md', readMeContent, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
---
+++ pageTemp.js
@@ -0,0 +1,3 @@
+//gen team function
+//inside gen team --gen manager, --gen engineer, --gen intern
+//module export 
