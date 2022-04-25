d882fabef26d55a8b81c46a5075369e88deb4937
Joshua Diaz <jad24096@gmail.com>
2022-04-25 15:11:13 -0400

    psuedo coded for next functions to create on the index.js.

--- index.js
+++ index.js
@@ -5,34 +5,49 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const path = require('path');
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateReadMe = ({fName, position, email}) => 
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'index.html');
 
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        message: 'What is your name?' ,
-        name: 'fName',
-    },
-    {
-        type: 'input',
-        message: 'What is your position?',
-        name: 'position',
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-    },
+const render = require('./pageTemp.js');
+
+const team = [];
+const teamId = [];
+
+const path = require('path');
+
+//large function for menu 
+//create manager function
+//add engineer function
+//add intern function
+//create team function
+//callback to create manager
+//callback function to trigger 
+
+
+
+// inquirer
+//   .prompt([
+//     {
+//         type: 'input',
+//         message: 'What is your name?' ,
+//         name: 'fName',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your position?',
+//         name: 'position',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your email?',
+//         name: 'email',
+//     },
     
 
-  ])
-  .then((data) => {
-    const readMeContent = generateReadMe(data)
-    fs.writeFile ('readme.md', readMeContent, (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
+//   ])
+//   .then((data) => {
+//     const readMeContent = generateReadMe(data)
+//     fs.writeFile ('readme.md', readMeContent, (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+//   });
